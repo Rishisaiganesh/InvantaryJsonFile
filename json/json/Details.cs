3fecc5432f07f1389c7dc37d38d13b77822c6f56
Rishisaiganesh <crazyganesh67@gmail.com>
2021-06-13 19:58:04 +0530

    [Rishi]Adding:Inventary Of Prodects Price

---
+++ json/json/Details.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Newtonsoft.Json.Linq;
+
+namespace json
+{
+    public class Details
+    {
+
+        public void GetData()
+        {
+            var json = File.ReadAllText(@"C:\Users\naren\source\Json\json\json\json1.json");
+            var jObject = JObject.Parse(json);
+            var ProdectsArray = (JArray)jObject["Prodects"];
+            if (jObject != null)
+            {
+                Console.WriteLine("Prodects");
+                foreach (var item in ProdectsArray)
+                {
+                    Console.WriteLine($"Name:" + (item["Name"]));
+                    Console.WriteLine($"Weight:" + (item["Weight"]));
+                    Console.WriteLine($"price:" + (item["Price_per_kg"]));
+                    Price(item["Weight"], item["Price_per_kg"]);
+                    Console.WriteLine();
+                }
+            }
+
+        }
+        public void Price(JToken Weight, JToken Price_per_kg)
+        {
+            int weight = Weight.ToObject<int>();
+            int price = Price_per_kg.ToObject<int>();
+            int total = weight * price;
+            Console.WriteLine("Total value of the iteam is" + total);
+        }
+    }
+
+        
+    
+}
---
+++ json/json/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace json
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Checking Prodects Price");
+            Details Data = new Details();
+            Data.GetData();
+        }
+    }
+}
